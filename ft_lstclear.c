037ffaa0b5241aa399a832fca1c24dcf8a3226c3
Anar-Erdene Bolor-Erdene <abolor-e@fu-r9-p2.s19.be>
2023-11-16 15:45:43 +0100

    f

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: abolor-e <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 12:06:07 by abolor-e          #+#    #+#             */
-/*   Updated: 2023/10/25 13:47:19 by abolor-e         ###   ########.fr       */
+/*   Updated: 2023/11/16 15:42:00 by abolor-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,4 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	(del)((*lst)->content);
 	free(*lst);
 	*lst = NULL;
-}
-
-void	del_content(void *content)
-{
-	free(content);
 }
